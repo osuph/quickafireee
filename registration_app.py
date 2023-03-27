30326c0afe23734b4ec13f23f0bb25c485490d9d
Ayase Minori <ayane@vignetteapp.org>
2023-03-27 17:12:38 +0000

    Add initial version of OAuth flow

--- registration_app.py
+++ registration_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
+import os
 from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(page_title="osu!Quickfire Registration",
     page_icon=":fire:", initial_sidebar_state="collapsed")
-
-# We are coming from an OAuth Callback so we must set our session states
 
 # HACK: This is to get rid of the sidebar
 no_sidebar_style = """
@@ -14,9 +13,37 @@ no_sidebar_style = """
 """
 st.markdown(no_sidebar_style, unsafe_allow_html=True)
 
+# PAGE SETUP ENDS HERE
 
-with open("REGISTRATION_INTRO.md", encoding="utf-8") as f:
-    st.markdown(f.read(), unsafe_allow_html=True)
+def create_authorization_url(client_id, redirect_uri, scope):
+    """ Returns the authorization URL for the osu! API"""
+    return f"https://osu.ppy.sh/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}"
 
-if st.button("Continue"):
-    switch_page("evaluation") # type:ignore
+
+def authed_main():
+    """ The main page for the app,  requires user to be authed firsthand """
+    with open("REGISTRATION_INTRO.md", encoding="utf-8") as f:
+        st.markdown(f.read(), unsafe_allow_html=True)
+
+    if st.button("Continue"):
+        switch_page("evaluation") # type:ignore
+
+if __name__ == "__main__":
+    # This is the client ID for the osu! API
+    client_id = 7270 or int(os.environ.get("CLIENT_ID"))
+    # This is the redirect URI for the osu! API
+    redirect_uri = "http://127.0.0.1:8501" or os.environ.get("REDIRECT_URI")
+    # This is the scope for the osu! API
+    scope = "public+identify"
+    # This is the authorization URL for the osu! API
+    auth_url = create_authorization_url(client_id, redirect_uri, scope)
+    # This is the authorization code for the osu! API
+    auth_code = st.experimental_get_query_params().get("code", None)
+
+    if auth_code:
+        authed_main()
+    else:
+        st.write(f"Please authorize this app to use your osu! account by clicking [here]({auth_url})")
+        st.write("You will be redirected to a page where you can authorize this app to access your osu! account.")
+        st.write("Once you have authorized this app, you will be redirected back to this page. Please click the button below to continue.")
+        st.write("If you are not redirected, please copy and paste the URL from your browser into the address bar of your browser.")
