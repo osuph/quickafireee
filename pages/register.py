24c5e5794a0df265e30020d765add735a4369a65
Ayase Minori <ayane@vignetteapp.org>
2023-03-29 05:08:44 +0000

    setup the variables for google sheets integration
    Signed-off-by: GitHub <noreply@github.com>

--- pages/register.py
+++ pages/register.py
@@ -15,18 +15,18 @@ with open("REGISTRATION_HEADER.md", encoding="utf-8") as f:
     st.markdown(f.read(), unsafe_allow_html=True)
 
 with st.form("registration_form"):
-    st.text_input("osu!username", st.session_state.get("osu_username", ""), 24)
-    st.text_input("Email", "", 24)
-    st.text_input("osu!profile URL", f"https://osu.ppy.sh/users/{st.session_state.get('osu_user_id', '')}")
+    osu_username = st.text_input("osu!username", st.session_state.get("osu_username", ""), 24)
+    email = st.text_input("Email", "", 24)
+    osu_url = st.text_input("osu!profile URL", f"https://osu.ppy.sh/users/{st.session_state.get('osu_user_id', '')}")
     # Since we may have some LAN tournaments, we can just toggle this and restart the application
     # Since we *might* reuse this beyond CONQUEST 2023
     if st.session_state["CONQUEST_MODE"]:
-        st.text_input("Proof of Attendance (Must be a Google Drive Link)", "")
-        st.multiselect("Days Attending", ["Day 1", "Day 2", "Day 3", "All Days"])
-        st.text_input("Phone Number", "")
+        conquest_proof = st.text_input("Proof of Attendance (Must be a Google Drive Link)", "")
+        days_attending = st.multiselect("Days Attending", ["Day 1", "Day 2", "Day 3", "All Days"])
+        phone_number = st.text_input("Phone Number", "")
     st.checkbox(
         "I consent to the collection of my data as per provisions of the Data Privacy Act of 2012.")
 
     if st.form_submit_button("Submit"):
         # Google forms logic goes here
-        st.success("Thank you for registering! A QR code has been provided.")
+        st.success("Thank you for registering! The tournament staff will get in touch with you soon.")
