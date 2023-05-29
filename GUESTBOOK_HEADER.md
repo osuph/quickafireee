1fa13b72dde6850889c4d62150f208c677e31acf
Ayase Minori <ayane@vignetteapp.org>
2023-05-29 14:00:21 +0000

    Explicitly define encoding
    Signed-off-by: GitHub <noreply@github.com>

--- GUESTBOOK_HEADER.md
+++ GUESTBOOK_HEADER.md
@@ -5,4 +5,4 @@
 TODO: have the GMT in the team write something here
 I'm fucking lazy, and an Ai cosplayer is holding me hostage right now
 
- -->
+-->
--- guestbook.py
+++ guestbook.py
@@ -3,7 +3,7 @@ import requests
 
 
 st.set_page_config(page_title="osu! booth Guestbook",
-    page_icon=":heart:", initial_sidebar_state="collapsed")
+    page_icon=":heart:")
 
 # HACK: This is to get rid of the sidebar
 no_sidebar_style = """
@@ -13,10 +13,12 @@ no_sidebar_style = """
 """
 st.markdown(no_sidebar_style, unsafe_allow_html=True)
 
+# Get header
+with open("GUESTBOOK_HEADER.md", "r", encoding="utf-8") as f:
+    st.markdown(f.read(), unsafe_allow_html=True)
 
 is_osu_user = st.checkbox("Are you an osu! player?")
 name = st.text_input("Your Name (doesn't have to be your real name)")
 days_attending = st.selectbox("What Day are you attending?",
                               ["Day 1", "Day 2", "Day 3", "All Days"])
 message = st.text_area("Leave a message for us!")
-
