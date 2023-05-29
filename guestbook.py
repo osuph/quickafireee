1edd54ea43bb55db1226c2e81cf1fcd4e44c4928
Frizu <39328974+KaidenFrizu@users.noreply.github.com>
2023-05-29 15:40:24 +0000

    Implement connection logic framework

--- guestbook.py
+++ guestbook.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 import requests
+from sheet_manager import sheet_manager
+from sheet_manager import servsecrets
 
 
 st.set_page_config(page_title="osu! booth Guestbook",
@@ -22,3 +25,24 @@ name = st.text_input("Your Name (doesn't have to be your real name)")
 days_attending = st.selectbox("What Day are you attending?",
                               ["Day 1", "Day 2", "Day 3", "All Days"])
 message = st.text_area("Leave a message for us!")
+
+if st.form_submit_button("Submit"):
+
+    # GSheets.sheets_key should be changed to another secret key-value pair
+    # To denote another GSheets file for Guest Logbook
+    manager = sheet_manager.SheetManager(
+        creds = servsecrets.service_acct_creds,
+        sheets_key = st.secrets.GSheets.sheets_key
+    )
+
+    data_dict = {
+        'is_osu_user':[is_osu_user],
+        'name':[name],
+        'days_attending':[days_attending],
+        'message':[message]
+    }
+
+    # The sheets must have 4 column names (as per keys in data_dict)
+    # that was made before pushing
+    manager.push(sheet_number=0, data=pd.DataFrame(data_dict))
+    st.success("Guestbook Sign-in Completed!")
