b75b7958f4da92730e52f8f8052f6a57ec6d4156
Ayase Minori <ayane@vignetteapp.org>
2023-05-31 01:31:08 +0000

    Make sure it fires only at match
    Signed-off-by: GitHub <noreply@github.com>

--- guestbook.py
+++ guestbook.py
@@ -59,7 +59,7 @@ with st.form('guestbook', clear_on_submit=True):
         }
 
         # Alert if someone special is here
-        if re.search(r'[VvIiNnCcEeNnT]*[0-9]{4}', name) or re.search(r'[TtoKkIiIiWwAa]', name):
+        if re.match(r'[VvIiNnCcEeNnT]*[0-9]{4}', name) is not None or re.match(r'[TtoKkIiIiWwAa]', name) is not None:
             # Fire the silent alarm
             post_to_webhook(
                 f"## :rotating_light: ALERT: Possible bad actor detected in Guestbook! ({name})")
