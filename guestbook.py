0f5c3904e7b11a43ce97b65a968e1850326d9eed
Ayase Minori <ayane@vignetteapp.org>
2023-05-31 01:05:29 +0000

    Allow form to clear itself after submit
    Signed-off-by: GitHub <noreply@github.com>

--- guestbook.py
+++ guestbook.py
@@ -21,7 +21,7 @@ def post_to_webhook(message):
 
 
 st.set_page_config(page_title="osu! booth Guestbook",
-    page_icon=":heart:", initial_sidebar_state="collapsed")
+                   page_icon=":heart:", initial_sidebar_state="collapsed")
 
 # HACK: This is to get rid of the sidebar
 no_sidebar_style = """
@@ -35,33 +35,35 @@ st.markdown(no_sidebar_style, unsafe_allow_html=True)
 with open("GUESTBOOK_HEADER.md", "r", encoding="utf-8") as f:
     st.markdown(f.read(), unsafe_allow_html=True)
 
-is_osu_user = st.checkbox("Are you an osu! player?")
-name = st.text_input("Your Name (doesn't have to be your real name)")
-days_attending = st.selectbox("What Day are you attending?",
+# Begin Guestbook Entrypoint
+with st.form('guestbook', clear_on_submit=True):
+    is_osu_user = st.checkbox("Are you an osu! player?")
+    name = st.text_input("Your Name (doesn't have to be your real name)")
+    days_attending = st.selectbox("What Day are you attending?",
                               ["Day 1", "Day 2", "Day 3", "All Days"])
-message = st.text_area("Leave a message for us!")
+    message = st.text_area("Leave a message for us!")
 
-if st.button("Submit"):
+    if st.form_submit_button("Submit"):
+        manager = sheet_manager.SheetManager(
+            creds=servsecrets.service_acct_creds,
+            sheets_key=st.secrets.GSheets.guestbook_sheets_key
+        )
 
-    manager = sheet_manager.SheetManager(
-        creds = servsecrets.service_acct_creds,
-        sheets_key = st.secrets.GSheets.guestbook_sheets_key
-    )
+        data_dict = {
+            'is_osu_user': [is_osu_user],
+            'name': [name],
+            'days_attending': [days_attending],
+            'message': [message]
+        }
 
-    data_dict = {
-        'is_osu_user':[is_osu_user],
-        'name':[name],
-        'days_attending':[days_attending],
-        'message':[message]
-    }
+        # Alert if someone special is here
+        if re.search(r'[VvIiNnCcEeNnT]*[0-9]{4}', name) or re.search(r'[TtoKkIiIiWwAa]', name):
+            # Fire the silent alarm
+            post_to_webhook(
+                f"## :rotating_light: ALERT: Possible bad actor detected in Guestbook! ({name})")
 
-    # Alert if someone special is here
-    if re.search(r'[VvIiNnCcEeNnT]*[0-9]{4}', name) or re.search(r'[TtoKkIiIiWwAa]', name):
-        # Fire the silent alarm
-        post_to_webhook(f"## :rotating_light: ALERT: Possible bad actor detected in Guestbook! ({name})")
-
-
-    # The sheets must have 4 column names (as per keys in data_dict)
-    # that was made before pushing
-    manager.push_data(sheet_number=0, data=pd.DataFrame(data_dict))
-    st.success(f'Thanks for signing to the Guestbook, {name}!')
+        # The sheets must have 4 column names (as per keys in data_dict)
+        # that was made before pushing
+        manager.push_data(sheet_number=0, data=pd.DataFrame(data_dict))
+        st.success(f'Thanks for signing to the Guestbook, {name}!')
+# End Guestbook Entrypoint
